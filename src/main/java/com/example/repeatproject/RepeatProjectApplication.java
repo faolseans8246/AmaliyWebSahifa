69a60faa4b7060650fa396052369f41a00a8ba3e
Feruzbek KHamroev <feruzbekkhamroev2002@gmail.com>
2023-10-29 17:24:02 +0500

    repeatProject loyihasini boshlash start version

---
+++ src/main/java/com/example/repeatproject/RepeatProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.repeatproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepeatProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepeatProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/repeatproject/RepeatProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.repeatproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepeatProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
